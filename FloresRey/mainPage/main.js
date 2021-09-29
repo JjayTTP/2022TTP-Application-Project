c7745554623b19a6fdf5a5d89edd9046c993a59a
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2021-09-29 16:30:52 -0400

    fixed profile image rotation

--- FloresRey/mainPage/main.js
+++ FloresRey/mainPage/main.js
@@ -1,11 +1,12 @@
-function loadName(){
+function loadProfileImg(){
     var profileImg = document.getElementById("profileImg");
     profileImg.style.right = "3%";
-    profileImg.style.transition = "right 1s";
+    profileImg.style.transform = "rotate(-360deg)";
+    profileImg.style.transition = "right 1s, transform 1s";
 }
 
 window.addEventListener("load",function(){
 
-    loadName();
+    // loadProfileImg();
 
 });
