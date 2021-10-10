d5ecbb1076dc681cad8a22479da1a6a58aa65bed
SQuinones <78192383+SQuinones@users.noreply.github.com>
2021-10-10 16:20:29 -0400

    adding some changes to navigation bar

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,13 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 50px";
+    document.getElementById("logo").style.fontSize = "35px";
   }
+}
