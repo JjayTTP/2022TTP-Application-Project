dcec991432d074cb290b532513efb30a0d0823fc
SQuinones <78192383+SQuinones@users.noreply.github.com>
2021-10-13 10:21:25 -0400

    adding resume file, images of my skills and adding skills section

--- myscripts.js
+++ myscripts.js
@@ -4,10 +4,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "20px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 50px";
+    document.getElementById("navbar").style.padding = "40px 20px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
