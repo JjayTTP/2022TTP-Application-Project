510f88d50087e1b8d75f99986bc8ecf4956c0369
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2021-10-14 19:16:42 -0400

    style.css

--- internproject/script.js
+++ internproject/script.js
@@ -2,6 +2,15 @@ const toggleButton = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".navbar-links");
 const logo = document.querySelector(".logo");
 
+// form-contact
+const form = document.querySelector(".form-contact-form");
+const mgSent = document.querySelector(".MgSent");
+
+form.addEventListener("submit", (e) => {
+  mgSent.innerHTML = "Message Sent !!!!";
+  e.preventDefault();
+});
+
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
