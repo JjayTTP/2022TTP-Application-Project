65de798f7280240928bf3bd0a60912ec6d946541
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2021-10-15 17:11:25 -0400

    fixed name animation and added top navigation

--- FloresRey/mainPage_Draft_3/main.js
+++ FloresRey/mainPage_Draft_3/main.js
@@ -6,8 +6,42 @@ function showProfileImg(){
     profileImg.style.transition = "left 1.3s ease, transform 1.3s ease";
 }
 
+function hideTopNav(){
+    var topNav = document.getElementsByClassName("topNav")[0];
+    var topName_Container = document.getElementsByClassName("topNav_name")[0];
+    var topName_Text = topName_Container.getElementsByTagName("h1")[0];
+    var topSelection = document.getElementsByClassName("topNav_options")[0];
+
+    topNav.style.height = "0";
+    topName_Container.style.top = "-60px";
+    topSelection.style.top = "-60px";
+
+    topName_Text.style.animation = "undoTyping";
+}
+function showTopNav(){
+    var topNav = document.getElementsByClassName("topNav")[0];
+    var topName_Container = document.getElementsByClassName("topNav_name")[0];
+    var topName_Text = topName_Container.getElementsByTagName("h1")[0];
+    var topSelection = document.getElementsByClassName("topNav_options")[0];
+
+    topNav.style.height = "60px";
+    topName_Container.style.top = "0";
+    topSelection.style.top = "0";
+
+    topName_Text.style.animation = "typing 1.2s steps(60, end), blink-caret-mini 1s step-end infinite";
+}
+
 window.addEventListener("load",()=>{
     setTimeout(()=>{
         showProfileImg();
     },4000)
+});
+
+window.addEventListener("scroll",()=>{
+    var scrollAmount = window.scrollY;
+    if(scrollAmount > 220){
+        showTopNav();
+    }else{
+        hideTopNav();
+    }
 });
