118f173b0a98fac4075838637a562b3b2211be7f
Michael Janus <michaeljanus61@gmail.com>
2021-10-16 18:58:05 -0400

    Added two new buttons and used JS to set the groundwork for hiding and showing content

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var aboutContent = document.getElementById('aboutContent');
+function showContact(){
+    aboutContent.style.display = 'none';
+ 
+}
+
+function showHome(){
+    aboutContent.style.display = 'flex';
+
+}
+
