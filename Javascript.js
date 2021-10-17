27d34a6d009384b809d3bc5610076471fe0b2d74
ChristopherThorne714 <christhorne714@gmail.com>
2021-10-17 11:47:45 -0400

    fixed some issues with the navbar
    still more to go

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
 function myFunction() {
     var x = document.getElementById("nav");
     if (x.className === "navbar") {
