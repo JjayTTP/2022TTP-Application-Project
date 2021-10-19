8ea4ad5a8682b1d222d968ef2ce53c265304a060
SQuinones <78192383+SQuinones@users.noreply.github.com>
2021-10-19 12:50:44 -0400

    adding button to hide and display nav menu

--- myscripts.js
+++ myscripts.js
@@ -12,7 +12,18 @@ function scrollFunction() {
   }
 }
 //-------------------
-
+// Function to toggle the bar
+function toggleMenu() {
+  var x = document.getElementById("navbar-right");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function myFunction(x) {
+  x.classList.toggle("fa-minus-circle");
+}
 
 
 
