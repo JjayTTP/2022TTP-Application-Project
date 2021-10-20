816c087665a1bbb56797b9c8cba31d22716c39f5
vincepogz <85046947+vincepogz@users.noreply.github.com>
2021-10-19 21:24:30 -0400

    added javascript

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTML/CSS/JS Test
+ Testing Environment for my learning purposes
+
+https://vincepogz.github.io/HTML-CSS-JS-Test/
+
---
+++ index.js
@@ -0,0 +1,81 @@
+function aboutButton()
+{
+    var element = document.getElementById("main");
+
+    if (element.style.display==="none")
+    {
+        element.style.display="block"
+        document.getElementById("Info").style.display="none"
+        document.getElementById("about").style.display="none" 
+    }
+
+    else
+    {
+        element.style.display="none"
+        document.getElementById("Info").style.display="block"
+        document.getElementById("about").style.display="block"     
+    }
+
+}
+
+function resumeButton()
+{
+    var element = document.getElementById("main");
+
+    if (element.style.display==="none")
+    {
+        element.style.display="block"
+        document.getElementById("Info").style.display="none"
+        document.getElementById("resume").style.display="none" 
+    }
+
+    else
+    {
+        element.style.display="none"
+        document.getElementById("Info").style.display="block"
+        document.getElementById("resume").style.display="block"     
+    }
+
+}
+
+function projectButton()
+{
+    var element = document.getElementById("main");
+
+    if (element.style.display==="none")
+    {
+        element.style.display="block"
+        document.getElementById("Info").style.display="none"
+        document.getElementById("project").style.display="none" 
+    }
+
+    else
+    {
+        element.style.display="none"
+        document.getElementById("Info").style.display="block"
+        document.getElementById("project").style.display="block"     
+    }
+
+}
+
+function contactButton()
+{
+    var element = document.getElementById("main");
+
+    if (element.style.display==="none")
+    {
+        element.style.display="block"
+        document.getElementById("Info").style.display="none"
+        document.getElementById("contact").style.display="none" 
+    }
+
+    else
+    {
+        element.style.display="none"
+        document.getElementById("Info").style.display="block"
+        document.getElementById("contact").style.display="block"     
+    }
+
+}
+
+
