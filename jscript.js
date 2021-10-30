ca6563baf3245b0482fdee4ce4613a96b0553762
Leandro Gamarra <71786791+r0m3c@users.noreply.github.com>
2021-10-30 18:10:47 -0400

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,8 @@
+/*Responsive Navigation*/
+	$(document).ready(function(){
+ $('.toggle').click(function(){
+	 $('.toggle').toggleClass('active')
+
+	 $('nav ul').toggleClass('active-menu')
+ })
+});
