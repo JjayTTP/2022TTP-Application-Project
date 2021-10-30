b90bda446b62bca214e54b10fc905944fcf28e43
Leandro Gamarra <71786791+r0m3c@users.noreply.github.com>
2021-10-30 18:28:54 -0400

    Delete jscript.js

--- jscript.js
+++
@@ -1,8 +0,0 @@
-/*Responsive Navigation*/
-	$(document).ready(function(){
- $('.toggle').click(function(){
-	 $('.toggle').toggleClass('active')
-
-	 $('nav ul').toggleClass('active-menu')
- })
-});
