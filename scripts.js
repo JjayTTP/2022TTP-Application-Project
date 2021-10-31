c41f0e750ef0adb1c44033872860c106737bb31b
Alex Brostowin <alexbrostowin@gmail.com>
2021-10-31 13:57:54 -0400

    Added icons and text to sections

--- scripts.js
+++ scripts.js
@@ -6,59 +6,81 @@ const body = document.querySelector('body');
 const button = document.querySelectorAll('button');
 const a = document.querySelectorAll('a');
 const theme_text = document.querySelector("#theme_text");
-const tab_1 = document.querySelector("#tab_1");
-const tab_2 = document.querySelector("#tab_2");
-const tab_3 = document.querySelector("#tab_3");
-const tab_4 = document.querySelector("#tab_4");
+const tabs = document.querySelectorAll(".tabs");
 const sections = document.querySelectorAll(".sections");
+const bio = document.querySelector("#bio");
+const skill_icons = document.querySelectorAll(".skill_icons");
 
+const tab_color_dark = ['#732222', '#263191', '#337982', '#1f692c'];
+const tab_color_dark_active = ['#a14c4c', '#4853b0', '#589aa3', '#3a8a48'];
+const tab_color_light = ['#f99', '#99afff', '#99e0ff', '#99ffaf'];
+const tab_color_light_active = ['#f74f4f', '#5779f7', '#55c3f2', '#4ef271']; 
+const palette_color_dark = ['#20232b','#272930', '#3b3e47'];
+const palette_color_light = ['white', '#d1d1d1', '#b3b3b3'];
 
 window.onload = function(){
 	ChangeTheme();//initialize to dark mode
-	ChangeTab(2);//show about section on load
+	ChangeTab(0);//show about section on load
 }
+
 function ChangeTab(id){
 	for (var i = 0; i < sections.length; i++){
 		if (i == id){
 			sections[i].style.display = 'initial';
+			if(dark_mode == 1){
+				tabs[i].style.backgroundColor = tab_color_dark_active[i];
+			}else{
+				tabs[i].style.backgroundColor = tab_color_light_active[i];
+			}
 		} else{
 			sections[i].style.display = 'none';
+			if(dark_mode == 1){
+				tabs[i].style.backgroundColor = tab_color_dark[i];
+			}else{
+				tabs[i].style.backgroundColor = tab_color_light[i];
+			}
 		}
-}
+	}
 }
 function ChangeTheme(){
 	if (dark_mode < 1){//enter dark mode
-		body.style.color = 'white';
-		body.style.backgroundColor = '#20232b';
+		body.style.color = 'white'; 
+		body.style.backgroundColor = palette_color_dark[0]; 
 		for (var i = 0; i < button.length; i++){
 			button[i].style.color = 'white';
 		}
 		for (var i = 0; i < a.length; i++){
 			a[i].style.color = 'white';
 		}
-		header_bar.style.backgroundColor = '#272930';
-		tab_1.style.backgroundColor = '#732222';
-		tab_2.style.backgroundColor = '#263191';
-		tab_3.style.backgroundColor = '#337982';
-		tab_4.style.backgroundColor = '#1f692c';
+		header_bar.style.backgroundColor = palette_color_dark[1]; 
+		for (var i = 0; i < tabs.length; i++){
+			tabs[i].style.backgroundColor = tab_color_dark[i];
+		}
+		bio.style.borderColor = palette_color_dark[2];
+		for (var i = 0; i < skill_icons.length; i++){
+			skill_icons[i].style.borderColor = palette_color_dark[2];
+		}
 		theme_text.style.backgroundColor = '#451d85';
 		theme_text.innerHTML = 'Dark Mode';
 		dark_mode = 1;
 	}
 	else{//enter light mode
 		body.style.color = 'black';
-		body.style.backgroundColor = 'white'; 
+		body.style.backgroundColor = palette_color_light[0]; 
 		for (i = 0; i < button.length; i++){
 			button[i].style.color = 'black';
 		}
 		for (i = 0; i < a.length; i++){
 			a[i].style.color = 'black';
 		}
-		header_bar.style.backgroundColor = '#d1d1d1';
-		tab_1.style.backgroundColor = '#f99';
-		tab_2.style.backgroundColor = '#99afff';
-		tab_3.style.backgroundColor = '#99e0ff';
-		tab_4.style.backgroundColor = '#99ffaf';
+		header_bar.style.backgroundColor = palette_color_light[1];
+		for (var i = 0; i < tabs.length; i++){
+			tabs[i].style.backgroundColor = tab_color_light[i];
+		}
+		bio.style.borderColor = palette_color_light[2];
+		for (var i = 0; i < skill_icons.length; i++){
+			skill_icons[i].style.borderColor = palette_color_light[1];
+		}
 		theme_text.style.backgroundColor = '#eff5b5';
 		theme_text.innerHTML = 'Light Mode';
 		dark_mode = 0;
