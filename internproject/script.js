f9e39945edb3fb3e11d96ee74257d5e92a085064
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2021-11-02 14:41:12 -0400

    pictures and hobbies

--- internproject/script.js
+++ internproject/script.js
@@ -1,6 +1,7 @@
 const toggleButton = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".navbar-links");
 const logo = document.querySelector(".logo");
+var myButton = document.getElementById("button");
 
 // form-contact
 const form = document.querySelector(".form-contact-form");
@@ -14,3 +15,23 @@ form.addEventListener("submit", (e) => {
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scroll > 100 || document.body.scrollTop < 100) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function showAndHideVideo() {
+  var video = document.getElementById("danceVideo");
+  if ((video.style.display = "none")) {
+    video.style.display = "block";
+  } else {
+    video.style.display = "none";
+  }
+}
