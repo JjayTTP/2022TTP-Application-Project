89743e876852653acbaf22d1e8749b8c77757d5d
Israel <israel.klein@jjay.cuny.edu>
2021-11-25 20:16:01 -0500

    gave event listener one more try using a different teuchnique it's just refusing to work. Will work on it again next week once I'm off work.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+document.querySelector("#button").addEventListener("click", changeRule);
+
+function changeRule() {
+    let stylesheet = document.styleSheets[0];
+    stylesheet.insertRule("div { color: blue; }");
+}
+
 
 let p = document.querySelector(".about");
 p.style = "color: maroon";
