f4882d245bcd5c272c27305cdf0d98b336774a14
Israel <israel.klein@jjay.cuny.edu>
2021-11-28 21:42:35 -0500

    added finishing touches + added another event listener

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-document.querySelector(".checkBtn").addEventListener("click", function () {
-   document.querySelector(".paragraph").style.color = "red";
+document.querySelector(".btn").addEventListener("click", function () {
+   document.querySelector(".paragraph").style.color = "aqua";
+
+});
+
+document.querySelector(".btnH").addEventListener("click", function () {
+    document.querySelector(".head").style.color = "white";
 
 });
 
