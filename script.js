a089d255017befda00a027be288bc1a6ac9586b5
Israel <israel.klein@jjay.cuny.edu>
2021-11-28 21:51:39 -0500

    added onother event handler to change text on click

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ document.querySelector(".btnH").addEventListener("click", function () {
     document.querySelector(".head").style.color = "white";
 
 });
+document.querySelector(".btnH2").addEventListener("click", function () {
+    document.querySelector(".head").innerHTML = "Thank you for interacting with my site :)";
+
+});
 
 
 
