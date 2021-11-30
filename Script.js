2f5b8dd647d6fc2d5671a0874905bc3a0239120f
syedak512 <syeda.kazmi93@bcmail.cuny.edu>
2021-11-29 19:18:06 -0500

    Made chnages

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
+console.log("Hello 🌎");
 
+/*
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the "Next steps" in the README
+*/
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
