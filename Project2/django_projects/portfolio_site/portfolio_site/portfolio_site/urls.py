c03691467b3e3c29a829341f543e63295416a5a4
codingsake <thinthazin.chris@gmail.com>
2022-01-14 10:21:46 -0800

    Added import os and Media path in settings.py

--- Project2/django_projects/portfolio_site/portfolio_site/portfolio_site/urls.py
+++ Project2/django_projects/portfolio_site/portfolio_site/portfolio_site/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about,name='about'),
+    # path('about/', views.about,name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
